31783c2f1f6c5af818bbc6fc45127cf54ec937b0
Claudia <claumonti@gmail.com>
2015-01-08 14:39:02 +0100

    test finally compiling

--- testperm.c
+++ testperm.c
@@ -1,8 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sunperf.h>
 
-#include <libmatmult.h>
+
+void matmult_lib(int m, int n, int k, double **A, double **B, double **C){
+	double alpha, beta;
+	char transa, transb;
+	
+	alpha = 1.0; beta = 0.0;
+	transa = 't', transb='t';
+  
+	dgemm(transa, transb, m, n, k, alpha, A[0], k, B[0], n, beta, C[0], m);
+		
+}
+
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( i = 0; i < m ; i++){
+		for ( j = 0; j < n; j++){
+			for ( t = 0; t < k ; t++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_mnk(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( i = 0; i < m ; i++){
+		for ( j = 0; j < n; j++){
+			for ( t = 0; t < k ; t++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( i = 0; i < m ; i++){
+		for ( t = 0; t < k ; t++){
+			for ( j = 0; j < n; j++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_nmk(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( j = 0; j < n; j++){
+		for ( i = 0; i < m ; i++){
+			for ( t = 0; t < k ; t++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_nkm(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( j = 0; j < n; j++){
+		for ( t = 0; t < k ; t++){
+			for ( i = 0; i < m ; i++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_knm(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( t = 0; t < k ; t++){
+		for ( j = 0; j < n; j++){
+			for ( i = 0; i < m ; i++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_kmn(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for ( t = 0; t < k ; t++){
+		for ( i = 0; i < m ; i++){
+			for ( j = 0; j < n; j++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+
+
 int main() {
+
 
 int m,n,k,i,j,l,sum=0; //dimensions
 double **A,**B,**C, *a, *b, *c; //matrices
@@ -76,7 +168,9 @@ for(i=0; i<m; i++){
 */
 
 
+matmult_kmn(m, n,  k,  A, B, C);
 
+matmult_lib( m,  n,  k, A, B,  C);
 for(i=0;i<m;i++){
   for(j=0;j<n;j++){
     printf("%d ",C[i][j]);
