3e0758b5404beb552e1a81db1a4489c18f05eff3
NathanQuadrio <nathan.quadrio@gmail.com>
2015-01-08 17:24:04 +0100

    up-to-date matmult_blk

--- natetest.c
+++ natetest.c
@@ -3,33 +3,6 @@
 #include <math.h>
 #include <sunperf.h>
 
-
-void matmult_lib(int m, int n, int k, double *A, double *B, double *C){
-	double alpha, beta;
-	char transa, transb;
-	
-	alpha = 1.0; beta = 0.0;
-	transa = 't', transb='t';
-  
-	dgemm(transa, transb, m, n, k, alpha, A, k, B, n, beta, C, m);
-		
-}
-
-void matmult_nat(int m, int n, int k, double *A, double *B, double *C){
-	int i,j,t;
-	// C++ printing commented out.-
-	for (i = 0; i < m ; i++){
-		for (j = 0; j < n; j++){
-			C[i*n+j]=0;
-			for (t = 0; t < k ; t++){
-				C[i*n+j] += A[i*k+t]*B[t*n+j];
-			}
-			//std::cout << C[i*n+j] << "\t";
-		}
-		//std::cout << std::endl;
-	}
-}
-
 int min(int num_a, int num_b){
 	if (num_a < num_b){
 		return num_a;
@@ -37,28 +10,11 @@ int min(int num_a, int num_b){
 	else return num_b;
 }
 
-void matrixing(int m, int n, double *a, double **A){
-	int i, j;
-	for (i=0; i<m; i++){
-		for (j=0; j<n; j++){
-			A[i][j] = a[j+i*n]; 
-		}
-	}
-}
-
-void arraying(int m, int n, double *a, double **A){
-	int i, j;
-	for (i=0; i<m; i++){
-		for (j=0; j<n; j++){
-			a[j+i*n] = A[i][j]; 
-		}
-	}
-}
-
 void matmult_blk_N(int m, int n, int l, double **A, double **B, double **C, int bb){
 	////// JUST FOR SQUARED MATRICES!
 	int i, j, k, ii, jj, kk;
 	double r;	
+	// getting the size of the block from the number of elements	
 	bb = sqrt(bb);
 	//blocked multiplication
 	for (kk = 0; kk < l; kk += bb){
