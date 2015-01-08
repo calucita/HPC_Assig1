4959c68bbe0992bea4a974749716bb41cf6d840c
InSannety <sanne.ae@gmail.com>
2015-01-08 11:37:25 +0100

    Update libmatmult.c
    Matrix form of nat, lib, and the nmk permutations

--- libmatmult.c
+++ libmatmult.c
@@ -27,4 +27,70 @@ void matmult_nat(int m, int n, int k, double **A, double **B, double **C){
 	}
 }
 
+void matmult_mnk(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int i = 0; i < m ; i++){
+		for (int j = 0; j < n; j++){
+			for (int t = 0; t < k ; t++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int i = 0; i < m ; i++){
+		for (int t = 0; t < k ; t++){
+			for (int j = 0; j < n; j++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_nmk(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int j = 0; j < n; j++){
+		for (int i = 0; i < m ; i++){
+			for (int t = 0; t < k ; t++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_nkm(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int j = 0; j < n; j++){
+		for (int t = 0; t < k ; t++){
+			for (int i = 0; i < m ; i++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_knm(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int t = 0; t < k ; t++){
+		for (int j = 0; j < n; j++){
+			for (int i = 0; i < m ; i++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
+void matmult_kmn(int m, int n, int k, double **A, double **B, double **C){
+	int i,j,t;
+	for (int t = 0; t < k ; t++){
+		for (int i = 0; i < m ; i++){
+			for (int j = 0; j < n; j++){
+				C[i][j] += A[i][t]*B[t][j];
+			}	
+		}
+	}
+}
+
 #endif
